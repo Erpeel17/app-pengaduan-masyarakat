21da587a4aa81ba8fec6b680c697c1f6d27dab87
rpl a angkatan 17 <rplalhuda17@gmail.com>
2023-07-31 09:58:31 +0700

    baru bikin model

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Complaint;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ComplaintController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('index', [
+            "user" => Auth::user()
+        ]);
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
 }
